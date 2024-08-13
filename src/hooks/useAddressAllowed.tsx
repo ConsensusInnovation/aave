38c2db2778a1cf38316b02ba2db240f84e1f3cc5
Yi-Cyuan Chen <emn178@gmail.com>
2024-08-13 10:32:27 +0800

    ansiable

--- src/hooks/useAddressAllowed.tsx
+++ src/hooks/useAddressAllowed.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
 
-import { usePolling } from './usePolling';
+// import { usePolling } from './usePolling';
 
 export interface AddressAllowedResult {
   isAllowed: boolean;
 }
 
-const TWO_MINUTES = 2 * 60 * 1000;
+// const TWO_MINUTES = 2 * 60 * 1000;
 
 export const useAddressAllowed = (address: string): AddressAllowedResult => {
   const [isAllowed, setIsAllowed] = useState(true);
